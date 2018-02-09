e15d01040d44454f2a44a540cb0c5941576fcfa4
zhangjunmin <zhangjunmin110@163.com>
2018-02-09 11:47:32 +0800

    '更新数据接口'

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
+// 路由
+const router = require('koa-router')();
+// 格式化参数
+const bodyParse = require('koa-bodyparser');
+
+// 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
 const Koa = require('koa');
-const fs = require('fs');
 const app = new Koa();
 
-app.use(ctx => {
-    ctx.body = fs.readFileSync('./views/index.html', 'utf8');
+app.use(bodyParser());
+// 列表
+router.get('/list', async (cxt, next) => {
+    cxt.response.body = '<h1>list</h1>';
+})
+// 详情
+router.get('/detail/:id', async (cxt, next) => {
+    var id = cxt.params.id;
+    cxt.response.body = `<h1>detail</h1><p>${id}</p>`;
+})
+// 首页
+router.get('/', async (cxt, next) => {
+    cxt.response.body = '<h1>Index</h1>';
+})
+
+
+app.use(router.routes());
+app.listen(3000, function() {
+    console.log('server is running！');
 });
-app.listen(3000);
